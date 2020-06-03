b975da05fa966c751c9c10b6da13dc4195b7beba
Tomas Causa <tomas@wcanvas.com>
2020-06-03 14:56:22 -0300

    Implementing special attack

--- app.js
+++ app.js
@@ -16,11 +16,18 @@ new Vue({
             if (this.checkWin()) {
                 return;
             }
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
+            this.monsterAttack();
         },
         specialAttack: function() {
-
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttack();
+        },
+        monsterAttack: function() {
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
         },
         heal: function() {
 
